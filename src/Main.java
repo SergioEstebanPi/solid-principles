6a443caba275fe89d526e5d5b8e611108e24f476
spina <spina@MCEX06X39.corporacionbci.cl>
2023-08-07 13:17:59 -0500

    Agrego estructura inicial

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        /*
+
+        This and example of the SOLID principles
+
+        S - Single responsibility principle
+        O - Open (extension) - Close (modification) principle
+        L - Liskov's sustitution principle
+        I - Interface segregation principle
+        D - Dependencie Inversion principle
+
+        */
+    }
+}
