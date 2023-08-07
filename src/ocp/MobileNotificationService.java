1f67d1a7a22a9b4307bf1ec40afbf0ae330bfeb2
spina <spina@MCEX06X39.corporacionbci.cl>
2023-08-07 13:43:14 -0500

    agrego open-closed principle

---
+++ src/ocp/MobileNotificationService.java
@@ -0,0 +1,15 @@
+package ocp;
+
+public class MobileNotificationService implements NotificationService {
+    @Override
+    public void sendOTP(String medium) {
+        // write logic to send otp to mobile
+        // twillio api
+    }
+
+    @Override
+    public void sendTransactionReport(String medium) {
+        // write logic to send otp to mobile
+        // twillio api
+    }
+}
---
+++ src/ocp/NotificationService.java
@@ -0,0 +1,8 @@
+package ocp;
+
+public interface NotificationService {
+
+    void sendOTP(String medium);
+
+    void sendTransactionReport(String medium);
+}
